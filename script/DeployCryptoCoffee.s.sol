037ebedd103835ada19ad26d37a9642bfbc6f759
shan5742 <asamshan456@gmail.com>
2023-11-03 15:29:17 +0000

    [readme] - init readme

--- script/DeployCryptoCoffee.s.sol
+++ script/DeployCryptoCoffee.s.sol
@@ -6,10 +6,10 @@ import {Script} from "forge-std/Script.sol";
 import {CryptoCoffee} from "../src/CryptoCoffee.sol";
 
 contract DeployCryptoCoffee is Script {
-  function run() external returns (CryptoCoffee) {
-    vm.startBroadcast();
-    CryptoCoffee cryptoCoffee = new CryptoCoffee();
-    vm.stopBroadcast();
-    return cryptoCoffee;
-  }
+    function run() external returns (CryptoCoffee) {
+        vm.startBroadcast();
+        CryptoCoffee cryptoCoffee = new CryptoCoffee();
+        vm.stopBroadcast();
+        return cryptoCoffee;
+    }
 }
--- src/CryptoCoffee.sol
+++ src/CryptoCoffee.sol
@@ -3,37 +3,37 @@
 pragma solidity ^0.8.19;
 
 contract CryptoCoffee {
-  address public owner;
-  uint256 public coffeeCount;
+    address public owner;
+    uint256 public coffeeCount;
 
-  event CoffeePurchased(address indexed buyer, uint256 amount);
-  event Withdrawn(address indexed withdrawer, uint256 amount);
+    event CoffeePurchased(address indexed buyer, uint256 amount);
+    event Withdrawn(address indexed withdrawer, uint256 amount);
 
-  constructor() {
-    // Set the contract creator as the owner
-    owner = msg.sender;
-  }
+    constructor() {
+        // Set the contract creator as the owner
+        owner = msg.sender;
+    }
 
-  modifier onlyOwner() {
-    require(msg.sender == owner, "Only the owner can call this function");
-    _;
-  }
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
 
-  // Function to buy a coffee
-  function buyCoffee() external payable {
-    require(msg.value > 0, "Coffee's aren't free!");
-    coffeeCount += 1;
+    // Function to buy a coffee
+    function buyCoffee() external payable {
+        require(msg.value > 0, "Coffee's aren't free!");
+        coffeeCount += 1;
 
-    emit CoffeePurchased(msg.sender, msg.value);
-  }
+        emit CoffeePurchased(msg.sender, msg.value);
+    }
 
-  function withdraw() external onlyOwner {
-    uint256 balance = address(this).balance;
-    require(balance > 0, "Balance is empty");
+    function withdraw() external onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "Balance is empty");
 
-    (bool success, ) = owner.call{value: balance}("");
-    require(success, "Failed to send Ether");
+        (bool success,) = owner.call{value: balance}("");
+        require(success, "Failed to send Ether");
 
-    emit Withdrawn(owner, balance);
-  }
+        emit Withdrawn(owner, balance);
+    }
 }
